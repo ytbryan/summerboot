7b549ebd0e9b6a1577dea61f3986f31e310d77d6
ytbryan <ytbryan@gmail.com>
2021-02-06 11:55:46 +0800

    Initial commit

---
+++ src/main/java/com/tada/summerboot/SummerbootApplication.java
@@ -0,0 +1,13 @@
+package com.tada.summerboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SummerbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SummerbootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tada/summerboot/SummerbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tada.summerboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SummerbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
