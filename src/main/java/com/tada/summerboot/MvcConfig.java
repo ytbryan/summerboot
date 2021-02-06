f8472edcd25de98c0ebfa60e7d3f85ffd5adf9df
ytbryan <ytbryan@gmail.com>
2021-02-06 23:59:12 +0800

    Added web security configuration, added viewController and  permit all API
    MVCConfig controls which url links to which view
    WebSecurityConfig controls which url needs to be authenticated

---
+++ src/main/java/com/tada/summerboot/MvcConfig.java
@@ -0,0 +1,16 @@
+package com.tada.summerboot;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class MvcConfig implements WebMvcConfigurer {
+	public void addViewControllers(ViewControllerRegistry registry) {
+		registry.addViewController("/hello").setViewName("hello");
+		registry.addViewController("/login").setViewName("login");
+		registry.addViewController("/error").setViewName("error");
+		registry.addViewController("/product").setViewName("product");
+
+	}
+}
--- src/main/java/com/tada/summerboot/WebSecurityConfig.java
+++ src/main/java/com/tada/summerboot/WebSecurityConfig.java
@@ -1,51 +1,52 @@
 package com.tada.summerboot;
 
-// Source https://spring.io/guides/gs/securing-web/
-// This is the configuration for authentication and authorisation
-// for the spring boot project
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
-@EnableWebSecurity //to enable Spring Security’s web security support and provide the Spring MVC integration.
-
+@EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http
-                .authorizeRequests()
-                .antMatchers("/", "/other.html", "/another.html").permitAll()
-                .anyRequest().authenticated()
-                .and()
-                .formLogin()
-                .loginPage("/login")
-                .permitAll()
-                .and()
-                .logout()
-                .permitAll();
-    }
+	@Override
+	public void configure(WebSecurity web) {
+		web.ignoring()
+				.antMatchers("/products/**")
+		        .antMatchers("/users/**");
+		//do not authenticate every APIs from restController
+	}
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http
+			.authorizeRequests()
+				.antMatchers("/").permitAll()
+				.antMatchers("/product").hasRole("ADMIN")
+				.anyRequest().authenticated()
+				.and()
+			.formLogin()
+				.loginPage("/login")
+				.permitAll()
+				.and()
+			.logout()
+				.permitAll();
+	}
 
-    @Bean
-    @Override
-    public UserDetailsService userDetailsService() {
-        UserDetails user =
-                User.withDefaultPasswordEncoder()
-                        .username("admin")
-                        .password("admin")
-                        .roles("USER", "ADMIN")
-                        .build();
+	@Bean
+	@Override
+	public UserDetailsService userDetailsService() {
+		UserDetails user =
+			 User.withDefaultPasswordEncoder()
+				.username("admin")
+				.password("admin")
+				.roles("ADMIN")
+				.build();
 
-        return new InMemoryUserDetailsManager(user);
-    }
+		return new InMemoryUserDetailsManager(user);
+	}
 }
