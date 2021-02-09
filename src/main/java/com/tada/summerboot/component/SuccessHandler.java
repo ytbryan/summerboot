ae24e3c64ada71b44adfc1bea3c2a49c8543be9c
ytbryan <ytbryan@gmail.com>
2021-02-09 22:27:12 +0800

    Added SuccessHandler component to redirect to home page after login

---
+++ src/main/java/com/tada/summerboot/component/SuccessHandler.java
@@ -0,0 +1,20 @@
+package com.tada.summerboot.component;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component
+public class SuccessHandler implements AuthenticationSuccessHandler {
+    @Override
+    public void onAuthenticationSuccess(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Authentication authentication) throws IOException, ServletException {
+        // After login, go to homepage
+        httpServletResponse.sendRedirect("/");
+    }
+}
--- src/main/java/com/tada/summerboot/security/WebSecurityConfig.java
+++ src/main/java/com/tada/summerboot/security/WebSecurityConfig.java
@@ -1,5 +1,7 @@
 package com.tada.summerboot.security;
 
+import com.tada.summerboot.component.SuccessHandler;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -14,6 +16,10 @@ import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Autowired
+	private SuccessHandler successHandler;
+
 	@Override
 	public void configure(WebSecurity web) {
 		//DO NOT EDIT
@@ -36,6 +42,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 			.formLogin()
 				.loginPage("/login")
 				.permitAll()
+				.successHandler(successHandler)
 				.and()
 			.logout()
 				.permitAll();
