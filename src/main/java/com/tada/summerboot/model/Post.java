2edb16f77b7bd316401ad5edad228eca6a87ef2d
ytbryan <ytbryan@gmail.com>
2021-02-15 09:00:21 +0800

    Update to Post.java and Product.java

--- src/main/java/com/tada/summerboot/model/Post.java
+++ src/main/java/com/tada/summerboot/model/Post.java
@@ -1,62 +1,68 @@
-//package com.tada.summerboot.model;
-//
-//import javax.persistence.Entity;
-//import javax.persistence.GeneratedValue;
-//import javax.persistence.GenerationType;
-//import javax.persistence.Id;
-//import java.math.BigDecimal;
-//
-//@Entity
-//public class Post {
-//
-//    @Id
-//    @GeneratedValue(strategy = GenerationType.TABLE) // GenerationType.TABLE will allow auto-increment of id
-//    private Integer id;
-//    private String title;
-//    private String content;
-//    private String imageURL;
-//
-//    public Post(Integer id, String title, String content, String imageURL) {
-//        this.id = id;
-//        this.title = title;
-//        this.content = content;
-//        this.imageURL = imageURL;
-//    }
-//
-//    public Post(){
-//        super();
-//    }
-//
-//    public Integer getId() {
-//        return id;
-//    }
-//
-//    public void setId(Integer id) {
-//        this.id = id;
-//    }
-//
-//    public String getTitle() {
-//        return title;
-//    }
-//
-//    public void setTitle(String title) {
-//        this.title = title;
-//    }
-//
-//    public String getContent() {
-//        return content;
-//    }
-//
-//    public void setContent(String content) {
-//        this.content = content;
-//    }
-//
-//    public String getImageURL() {
-//        return imageURL;
-//    }
-//
-//    public void setImageURL(String imageURL) {
-//        this.imageURL = imageURL;
-//    }
-//
-//}
+package com.tada.summerboot.model;
+
+import javax.persistence.*;
+
+@Entity
+public class Post {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.TABLE) // GenerationType.TABLE will allow auto-increment of id
+    private Integer id;
+    private String title;
+    private String content;
+    private String imageURL;
+
+    // Required for the @OneToMany relationship with a User
+    @Column(name = "user_id")
+    private Integer user_id;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getImageURL() {
+        return imageURL;
+    }
+
+    public void setImageURL(String imageURL) {
+        this.imageURL = imageURL;
+    }
+
+    public Post(Integer id, String title, String content, String imageURL) {
+        this.id = id;
+        this.title = title;
+        this.content = content;
+        this.imageURL = imageURL;
+    }
+
+    public Post(String title, String content, String imageURL) {
+        this.title = title;
+        this.content = content;
+        this.imageURL = imageURL;
+    }
+
+    public Post(){
+        super();
+    }
+
+}
--- src/main/java/com/tada/summerboot/model/Product.java
+++ src/main/java/com/tada/summerboot/model/Product.java
@@ -1,13 +1,11 @@
 package com.tada.summerboot.model;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.math.BigDecimal;
 
 @Entity
 public class Product {
+
     @Id
     @GeneratedValue(strategy = GenerationType.TABLE) // GenerationType.TABLE will allow auto-increment of id
     private Integer id;
@@ -16,18 +14,14 @@ public class Product {
     private String sku;
     private String title;
     private String description;
+    private String productType;
 
-    public Product(BigDecimal price, Integer quantity, String sku, String title, String description) {
-        this.price = price;
-        this.quantity = quantity;
-        this.sku = sku;
-        this.title = title;
-        this.description = description;
-    }
+    @Column(nullable = true, length = 64)
+    private String imageURL;
 
-    public Product(){
-        super();
-    }
+    // Required for the @OneToMany relationship with a User
+    @Column(name = "user_id")
+    private Integer user_id;
 
     public Integer getId() {
         return id;
@@ -51,5 +45,87 @@ public class Product {
 
     public String getDescription() {
         return description;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+
+    public void setSku(String sku) {
+        this.sku = sku;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getImageURL() {
+        return imageURL;
+    }
+
+    public Integer getUser_id() {
+        return user_id;
+    }
+
+    public void setUser_id(Integer user_id) {
+        this.user_id = user_id;
+    }
+
+    public void setImageURL(String imageURL) {
+        this.imageURL = imageURL;
+    }
+
+    public Product(BigDecimal price, Integer quantity, String sku, String title, String description, Integer user_id) {
+        this.price = price;
+        this.quantity = quantity;
+        this.sku = sku;
+        this.title = title;
+        this.description = description;
+        this.user_id = user_id;
+    }
+
+    public Product(Integer id, BigDecimal price, Integer quantity, String sku, String title, String description, Integer user_id) {
+        this.id = id;
+        this.price = price;
+        this.quantity = quantity;
+        this.sku = sku;
+        this.title = title;
+        this.description = description;
+        this.user_id = user_id;
+    }
+
+    public Product(Integer id, BigDecimal price, Integer quantity, String sku, String title, String description) {
+        this.id = id;
+        this.price = price;
+        this.quantity = quantity;
+        this.sku = sku;
+        this.title = title;
+        this.description = description;
+    }
+
+    public Product(BigDecimal price, Integer quantity, String sku, String title, String description) {
+        this.price = price;
+        this.quantity = quantity;
+        this.sku = sku;
+        this.title = title;
+        this.description = description;
+    }
+
+    public Product(){
+        super();
+    }
+
+    @Override
+    public String toString() {
+        return "Product [id=" + id + ", title=" + title +
+                ", description=" + description + ", price=" + price + ", quantity=" + quantity + ", sku=" + sku + "]";
     }
 }
