8096555004146d590b086f4513e130b934c938dd
ytbryan <ytbryan@gmail.com>
2021-02-15 09:02:09 +0800

    Update ProductRepository.java
    Find a list of products own by a specific user

--- src/main/java/com/tada/summerboot/repository/ProductRepository.java
+++ src/main/java/com/tada/summerboot/repository/ProductRepository.java
@@ -1,9 +1,17 @@
 package com.tada.summerboot.repository;
 
 import com.tada.summerboot.model.Product;
+import com.tada.summerboot.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
-public interface ProductRepository extends JpaRepository<Product, String> {
+import java.util.List;
+
+public interface ProductRepository extends JpaRepository<Product, Integer> {
     //This interface is used to save/update/read/delete data at`product` table
     // It ships with methods like .save() or .findById()
+
+    //findAllProductByUserId - give the user id and get all the user's products
+    @Query("SELECT n FROM Product n WHERE n.user_id = ?1")
+    List<Product> findAllProductsByUserId(Integer user_id);
 }
