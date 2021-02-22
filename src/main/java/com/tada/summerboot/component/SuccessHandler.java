59d6564bc498952ad31a5e3400f9076d27f03fce
ytbryan <ytbryan@gmail.com>
2021-02-22 13:04:46 +0800

    This is a first draft of multiple user role (admin, user)

--- src/main/java/com/tada/summerboot/component/SuccessHandler.java
+++ src/main/java/com/tada/summerboot/component/SuccessHandler.java
@@ -1,6 +1,8 @@
 package com.tada.summerboot.component;
 
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
 
@@ -8,6 +10,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Iterator;
 
 @Component
 public class SuccessHandler implements AuthenticationSuccessHandler {
@@ -15,7 +18,33 @@ public class SuccessHandler implements AuthenticationSuccessHandler {
     public void onAuthenticationSuccess(HttpServletRequest httpServletRequest,
                                         HttpServletResponse httpServletResponse,
                                         Authentication authentication) throws IOException, ServletException {
-        // After login, go to homepage
+
+//        UNCOMMENT SNIPPET #2
+
+//        This snippet of code will allow user to
+//        go to one page and admin to another page.
+//
+//
+
+//        boolean isAdmin = false;
+//        UserDetails user = (UserDetails) authentication.getPrincipal();
+//        Iterator<org.springframework.security.core.GrantedAuthority> grantedAuthorityIterator = (Iterator<GrantedAuthority>) user.getAuthorities().iterator();
+//        while (grantedAuthorityIterator.hasNext()) {
+//            if (grantedAuthorityIterator.next().getAuthority().equalsIgnoreCase("ADMIN")) {
+//                isAdmin = true;
+//            }
+//        }
+//
+//        if (isAdmin) { // if isAdmin is true == true.
+//            System.out.println("reached /admin");
+//            httpServletResponse.sendRedirect("/admin");
+//        } else { // if it is false.
+//            System.out.println("reached /");
+//            httpServletResponse.sendRedirect("/");
+//        }
+
+
         httpServletResponse.sendRedirect("/");
+
     }
 }
--- src/main/java/com/tada/summerboot/security/WebSecurityConfig.java
+++ src/main/java/com/tada/summerboot/security/WebSecurityConfig.java
@@ -6,6 +6,7 @@ import com.tada.summerboot.service.UserServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -33,9 +34,12 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 		//DO NOT EDIT
 		//do not authenticate these APIs
 		web.ignoring()
+//				.antMatchers("/contact.html")
+
 				.antMatchers("/css/**")
 				.antMatchers("/js/**")
 				.antMatchers("/images/**")
+				.antMatchers("/every-users")
 
 				.antMatchers("/user-photos/**")
 				.antMatchers("/products/**")
@@ -49,6 +53,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 		http
 			.authorizeRequests()
 				.antMatchers("/").permitAll()
+				.antMatchers("/register").permitAll()
+				.antMatchers(HttpMethod.POST, "/user/new").permitAll()
 				.antMatchers("/product").hasRole("ADMIN")
 				.anyRequest().authenticated()
 				.and()
@@ -58,8 +64,15 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 				.successHandler(successHandler)
 				.and()
 			.logout()
+				.logoutSuccessUrl("/") //Set the url after you logout
 				.permitAll();
 	}
+
+
+
+	// This method will only be loaded once when you start the server
+	// It assumes you should have users in your database
+	// Read more https://www.javadevjournal.com/spring/spring-security-userdetailsservice/
 
 	@Bean
 	@Override
@@ -72,7 +85,6 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 		// Prepare an ArrayList for the InMemoryUserDetailsManager method at the end of this function
 		ArrayList<UserDetails> list = new ArrayList<UserDetails>();
 
-		// Iterate (go through one by one) and build a UserDetails for this app
 		for (int i = 0; i < users.size(); i++) {
 
 			// Create a UserDetails instance but set it based on the user in database
@@ -86,16 +98,74 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 			list.add(user);
 		}
 
-		//Have at least one admin user for developer to login
+		list = adding_super_user(list);
+
+		return new InMemoryUserDetailsManager(list);
+	}
+
+
+	//        UNCOMMENT SNIPPET #1
+	// Remember to comment out the userDetailsService Above.
+
+//	@Bean
+//	@Override
+//	public UserDetailsService userDetailsService() {
+//		// Get all the users in database
+//		// Use the service instead of repo.
+//		List<com.tada.summerboot.model.User> users = user_service_implementation.getAllUsers();
+//
+//		// Prepare an ArrayList for the InMemoryUserDetailsManager method at the end of this function
+//		ArrayList<UserDetails> list = new ArrayList<UserDetails>();
+//
+//		// Iterate (go through one by one) and build a UserDetails for this app
+//		for(int i = 0; i < users.size(); i++) {
+//			if(users.get(i).getUserType() != null){ // check if UserType is null
+//				list = set_user_type(list, users.get(i));
+//
+//				// you must make sure there is only two possibilities
+//			} else {
+//				// Create a UserDetails instance but set it based on the user in database
+//				UserDetails user = User.withDefaultPasswordEncoder()
+//						.username(users.get(i).getUsername())
+//						.password(users.get(i).getPassword())
+//						.roles("ADMIN")
+//						.build();
+//
+//				// Add that instance to the list
+//				list.add(user);
+//			}
+//		}
+//
+//		list = adding_super_user(list);
+//		//Have at least one admin user for developer to login
+//		return new InMemoryUserDetailsManager(list);
+//	}
+////
+//	private ArrayList<UserDetails>  set_user_type(ArrayList<UserDetails> list, com.tada.summerboot.model.User this_user) {
+//		UserDetails user = User.withDefaultPasswordEncoder()
+//				.username(this_user.getUsername())
+//				.password(this_user.getPassword())
+//				.roles(this_user.getUserType())
+//				.build(); // This assumes that getUserType is either ADMIN or USER
+//
+//		list.add(user);
+//		return list;
+//	}
+
+	private ArrayList<UserDetails> adding_super_user(ArrayList<UserDetails> list) {
 		UserDetails admin =
 				User.withDefaultPasswordEncoder()
 						.username("admin")
 						.password("admin")
 						.roles("ADMIN")
 						.build();
-		list.add(admin);
 
-		System.out.println(list);
-		return new InMemoryUserDetailsManager(list);
+		list.add(admin);
+		return list;
 	}
+
+
+
+
+
 }
