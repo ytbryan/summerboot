e8bb787a8ef8270ea22e05fd47e92aed804db482
Kimberly Wan <kimberly.wanst@gmail.com>
2021-03-20 21:54:56 +0800

    Update post model and post controller

--- src/main/java/com/tada/summerboot/controller/PostController.java
+++ src/main/java/com/tada/summerboot/controller/PostController.java
@@ -14,6 +14,9 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
 import java.util.Optional;
 
@@ -26,30 +29,51 @@ public class PostController {
     @Autowired
     UserServiceImpl user_service_implementation;
 
-    @GetMapping(path="/post/show-username/{id}", produces = { MediaType.APPLICATION_JSON_VALUE })
+    @GetMapping(path="/posts/show-username/{id}")
     public String showUserName(Model model, @PathVariable("id") Integer id) {
+        System.out.println("reached here");
         Optional <Post> post = post_service_implementation.getPost(id);
         model.addAttribute("post", post.get());
         Optional <User> user = user_service_implementation.getUser( post.get().getUser_id());
         model.addAttribute("user", user.get());
-        //$(user.username)?
-        return "examples/show-post-with-username";
+        return "show-post";
     }
+
+//    @GetMapping(path="/post/show-username/{id}", produces = { MediaType.APPLICATION_JSON_VALUE })
+//    public String showUserName(Model model, @PathVariable("id") Integer id) {
+//        Optional <Post> post = post_service_implementation.getPost(id);
+//        model.addAttribute("post", post.get());
+//        Optional <User> user = user_service_implementation.getUser( post.get().getUser_id());
+//        model.addAttribute("user", user.get());
+//        //$(user.username)?
+//        return "show-post-with-username";
+//    }
 
     @GetMapping(value="post-image") // it will be set to be /product
     public String postWithImage(Model model){
         User user = user_service_implementation.current_user();
         model.addAttribute("user", user);
         model.addAttribute("post", new Post());
-        return "examples/post-image";
+        return "publish";
     }
 
-    @GetMapping(value="/every-posts-no-table")
+    @GetMapping(value="/")
     public String everypostWithoutTable(Model model){
-        User user = user_service_implementation.current_user();
-        List<Post> list = post_service_implementation.findAllByUserId(user.getId());
-        model.addAttribute("posts", list);
-        return "examples/every-posts-no-table";
+        System.out.println("reached here /");
+        List<Post> posts = post_service_implementation.getAllPosts(); // get all the posts
+        model.addAttribute("posts", posts);
+
+        List<User> users = new ArrayList<>(); // prepare an empty list
+        System.out.println(posts.size());
+        for (int i = 0; i < posts.size(); i++) {
+            Post post = posts.get(i);
+            Optional<User> user = user_service_implementation.getUser(post.getUser_id()); // get the user object
+            users.add(user.get()); // add it to the userList
+        }
+
+        model.addAttribute("users", users); // make it available via users variable. Use stats.index to access specific index.
+
+        return "index";
     }
 
     @GetMapping(value="/every-posts-by-single-user")
@@ -57,34 +81,48 @@ public class PostController {
         User user = user_service_implementation.current_user();
         List<Post> list = post_service_implementation.findAllByUserId(user.getId());
         model.addAttribute("posts", list);
-        return "examples/every-posts-by-single-user";
+        model.addAttribute("user", user);
+        return "profile";
     }
 
-    @GetMapping(value="/every-posts") // it will be set to be /product
+//    @GetMapping(value="/individual-post")
+//    public String everypostByIndividual(Model model){
+//        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+//        User user = user_service_implementation.current_user(auth.getName());
+//        List<Post> list = post_service_implementation.findAllByUserId(user.getId());
+//        model.addAttribute("posts", list);
+//        return "individual-post";
+//    }
+
+    @GetMapping(value="/") // it will be set to be /product
     public String everyposts(Model model){
         List<Post> posts = post_service_implementation.getAllPosts();
+        List<User> users = new ArrayList<>();
+
+
         model.addAttribute("posts", posts); // this will pass the value to a ${user}
         return "examples/every-posts";
     }
 
-    @GetMapping(value="/post") // it will be set to be /product
-    public String post(Model model){
-        User user = user_service_implementation.current_user();
-
-        model.addAttribute("user", user);
-        model.addAttribute("post", new Post());
-        return "examples/post";
-    }
+//    @GetMapping(value="/post") // it will be set to be /product
+//    public String post(Model model){
+//        User user = user_service_implementation.current_user();
+//
+//        model.addAttribute("user", user);
+//        model.addAttribute("post", new Post());
+//        return "examples/post";
+//    }
 
     @PostMapping(path="post/image/new")
-    public String newPostWithImage(@RequestParam(name="title", required = false) String title,
+    public String newPostWithImage(@RequestParam(name="id", required = false) Integer id,
+                                     @RequestParam(name="title", required = false) String title,
                                       @RequestParam(name="content", required = false) String content,
                                       @RequestParam(name="user_id", required = false) Integer user_id,
                                       @RequestParam(name="image") MultipartFile multipartFile) throws IOException {
 
-        Post new_post = new Post(title, content, user_id, multipartFile.getBytes());
+        Post new_post = new Post(id, title, content, user_id, multipartFile.getBytes(), new Timestamp(Calendar.getInstance().getTime().getTime()));
         post_service_implementation.createOrUpdatePost(new_post);
-        return "examples/every-posts";
+        return "redirect:/every-posts-by-single-user";
     }
 
     @GetMapping(path="/post/all", produces = { MediaType.APPLICATION_JSON_VALUE })
@@ -94,16 +132,16 @@ public class PostController {
     }
 
     // this is for form-data
-    @PostMapping(path="/post/new")
-    public String newPost(@RequestParam(name="id", required=false) Integer id,
-                          @RequestParam(name="title", required = false) String title,
-                          @RequestParam(name="content", required = false) String content,
-                          @RequestParam(name="user_id", required = false) Integer user_id,
-                          @RequestParam(name="imageURL", required = false) String imageURL) {
-
-        post_service_implementation.createOrUpdatePost(new Post(id, title, content, user_id, imageURL));
-        return "examples/every-posts";
-    }
+//    @PostMapping(path="/post/new")
+//    public String newPost(@RequestParam(name="id", required=false) Integer id,
+//                          @RequestParam(name="title", required = false) String title,
+//                          @RequestParam(name="content", required = false) String content,
+//                          @RequestParam(name="user_id", required = false) Integer user_id,
+//                          @RequestParam(name="imageURL", required = false) String imageURL) {
+//
+//        post_service_implementation.createOrUpdatePost(new Post(id, title, content, user_id, imageURL));
+//        return "examples/every-posts";
+//    }
 
     //this is for javascript
     @PostMapping(path="/post/json/new", produces = { MediaType.APPLICATION_JSON_VALUE })
@@ -113,19 +151,21 @@ public class PostController {
         return "{\"status\": \"success\"}";
     }
 
-    @GetMapping(path="/post/show/{id}", produces = { MediaType.APPLICATION_JSON_VALUE })
-    public String show(Model model, @PathVariable("id") Integer id) {
-        Optional <Post> post = post_service_implementation.getPost(id);
-        model.addAttribute("post", post);
-        return "examples/show-post";
-    }
+//    @GetMapping(path="/post/show/{id}", produces = { MediaType.APPLICATION_JSON_VALUE })
+//    public String show(Model model, @PathVariable("id") Integer id) {
+//        Optional <Post> post = post_service_implementation.getPost(id);
+//        model.addAttribute("post", post);
+//        return "examples/show-post";
+//    }
 
     //TODO - change to delete
     @GetMapping(path="/post/delete/{id}")
     public String destroy(@PathVariable("id") Integer id) {
         post_service_implementation.deletePost(id);
-        return "examples/every-posts";
+        return "redirect:/every-posts-by-single-user";
     }
+
+
 
     @RequestMapping(path = {"post/edit", "post/edit/{id}"})
     public String editPost(Model model, @PathVariable("id") Integer id)
@@ -141,6 +181,10 @@ public class PostController {
             System.out.println("The id is NULL");
             model.addAttribute("post", new Post());
         }
-        return "examples/post";
+        return "publish";
     }
+
+
 }
+
+//line 88 edited
--- src/main/java/com/tada/summerboot/model/Post.java
+++ src/main/java/com/tada/summerboot/model/Post.java
@@ -1,11 +1,11 @@
 package com.tada.summerboot.model;
 
-import javax.persistence.*;
-
-
-import lombok.Setter;
 import lombok.Getter;
+import lombok.Setter;
 
+import javax.persistence.*;
+import java.sql.Timestamp;
+import java.time.ZonedDateTime;
 
 @Getter
 @Setter
@@ -15,16 +15,84 @@ public class Post {
     @Id
     @GeneratedValue(strategy = GenerationType.TABLE) // GenerationType.TABLE will allow auto-increment of id
     private Integer id;
+
+    @Column(nullable = true) // allow null value in this column
     private String title;
+
+    @Column(nullable = true, columnDefinition="mediumblob")
+    private byte[] image;
+
+    @Column(nullable = true, columnDefinition="TEXT") // allow null value in this column
     private String content;
+
+    @Column(nullable = true) // allow null value in this column
     private String imageURL;
+
+
+    @Column(nullable = true) // allow null value in this column
+    private Timestamp timestamp;
+
+    public Post(Integer id, String title, String content, Integer user_id, byte[] bytes, Timestamp timestamp) {
+        this.id = id;
+        this.title = title;
+        this.content = content;
+        this.user_id = user_id;
+        this.image = bytes;
+        this.timestamp = timestamp;
+
+    }
+
+    public Timestamp getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(Timestamp timestamp) {
+        this.timestamp = timestamp;
+    }
 
     // Required for the @OneToMany relationship with a User
     @Column(name = "user_id")
     private Integer user_id;
 
-    @Column(nullable = true, columnDefinition="mediumblob")
-    private byte[] image;
+    public Integer getUser_id() {
+        return user_id;
+    }
+
+    public void setUser_id(Integer user_id) {
+        this.user_id = user_id;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getImageURL() {
+        return imageURL;
+    }
+
+    public void setImageURL(String imageURL) {
+        this.imageURL = imageURL;
+    }
 
     public Post(Integer id, String title, String content, String imageURL) {
         this.id = id;
@@ -49,13 +117,6 @@ public class Post {
         this.user_id = user_id;
     }
 
-    public Post(String title, String content, Integer user_id, byte[] image) {
-        this.title = title;
-        this.content = content;
-        this.user_id = user_id;
-        this.image = image;
-    }
-
     public Post(String title, String content, Integer user_id, String imageURL) {
         this.title = title;
         this.content = content;
@@ -63,12 +124,20 @@ public class Post {
         this.imageURL = imageURL;
     }
 
-    public Post(Integer id, String title, String content, Integer user_id, String imageURL) {
-        this.id = id;
+    public Post(String title, String content, Integer user_id, String imageURL, Timestamp timestamp) {
         this.title = title;
         this.content = content;
         this.user_id = user_id;
         this.imageURL = imageURL;
+        this.timestamp = timestamp;
     }
+
+    public Post(String title, String content, Integer user_id, Timestamp timestamp) {
+        this.title = title;
+        this.content = content;
+        this.user_id = user_id;
+        this.timestamp = timestamp;
+    }
+
 
 }
